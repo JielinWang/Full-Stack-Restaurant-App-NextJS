5d53148d09eef6e178ed496a8d523e9716a1132d
jielin Wang <wangjielin777@gmail.com>
2022-12-13 19:58:01 -0600

    create-strapi-app

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3627dd047114784daf1352c88bce3ae2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4e04392a-6c44-47c3-b714-c5e9949e44fa'
+};
